0c502a7ad230a5d8d5299a6a78f768ea75518b84
Elisenda-LV <elisenda.lv@gmail.com>
2024-10-11 10:48:29 +0200

    models updated

--- src/models/leagues.models.ts
+++ src/models/leagues.models.ts
@@ -8,37 +8,45 @@ import Team from './teams.model';
 const League = db.define('League', {
 
     id_league: {
-        type: DataTypes.INTEGER,
+        type: DataTypes.UUID,
         primaryKey: true,
-        autoIncrement: true,
-        allowNull: false,
-    },
-    sport_id: {
-        type: DataTypes.MEDIUMINT,
+        defaultValue: DataTypes.UUIDV4,
         allowNull: false,
     },
     user_id: {
-        type: DataTypes.MEDIUMINT,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
         allowNull: false,
     },
     league_name: {
         type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+
+    },
+    sport: {
+        type: DataTypes.STRING,
+        allowNull: false,
 
     },
     gender: {
         type: DataTypes.STRING,
+        allowNull: false,
 
     },
     category: {
         type: DataTypes.STRING,
+        allowNull: false,
 
     },
     description: {
         type: DataTypes.STRING,
+        allowNull: false,
 
     },
     location: {
         type: DataTypes.STRING,
+        allowNull: false,
 
     },
     create_at: {
--- src/models/teams.model.ts
+++ src/models/teams.model.ts
@@ -7,42 +7,44 @@ class Team extends Model {}
 Team.init({
     
     id_team: {
-        type: DataTypes.MEDIUMINT,
+        type: DataTypes.UUID,
         primaryKey: true,
-        autoIncrement: true,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
     },
     user_id: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: 'User', //users.model
-            key: 'id_user',     
-        },
-    },
-    sport_id: {
-        type: DataTypes.MEDIUMINT,
-        references: {
-            model: 'Sport', //sports.model
-            key: 'id_sport',  
-        },
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
     },
     team_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    sport: {
         type: DataTypes.STRING,
         allowNull: false,
     },
     gender: {
         type: DataTypes.STRING,
+        allowNull: false,
     },
     category: {
         type: DataTypes.STRING,
+        allowNull: false,
     },
     location: {
         type: DataTypes.STRING,
+        allowNull: false,
     },
     description: {
         type: DataTypes.STRING,
+        allowNull: false,
     },
     create_at: {
         type: DataTypes.DATE,
+        
     },
     updated_at: {
         type: DataTypes.DATE,
